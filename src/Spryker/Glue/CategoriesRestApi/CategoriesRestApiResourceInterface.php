307a024c0784a7ed871570357a5ddfcb5518b15c
voitov <tigran.voitov@spryker.com>
2018-08-21 19:38:17 +0300

    GLUE-780 Relationship module's implementation, categoriesRestApi editions

---
+++ src/Spryker/Glue/CategoriesRestApi/CategoriesRestApiResourceInterface.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Glue\CategoriesRestApi;
+
+use Spryker\Glue\GlueApplication\Rest\JsonApi\RestResourceInterface;
+use Spryker\Glue\GlueApplication\Rest\Request\Data\RestRequestInterface;
+
+interface CategoriesRestApiResourceInterface
+{
+    /**
+     * Specification:
+     * - Retrieves abstract product categories by abstract product identifier.
+     *
+     * @api
+     *
+     * @param \Spryker\Glue\GlueApplication\Rest\Request\Data\RestRequestInterface $restRequest
+     *
+     * @return \Spryker\Glue\GlueApplication\Rest\JsonApi\RestResourceInterface|null
+     */
+    public function readProductCategories(string $idAbstractProduct, RestRequestInterface $restRequest): ?RestResourceInterface;
+}
